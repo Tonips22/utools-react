2702ec54f59fd7497b4fc0c64a4679fb988c47f1
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-01-19 14:36:48 +0100

    Add absolute routes configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@sections': path.resolve(__dirname, 'src/sections'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+});
