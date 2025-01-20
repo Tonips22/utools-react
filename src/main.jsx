825c758f0ab890f5b89bf749d5a49c60752d6086
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-01-20 20:49:18 +0100

    Add fountsources

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
 import App from './App.jsx'
+import '@fontsource/inder';
+import '@fontsource-variable/inter';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
