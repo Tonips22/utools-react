b44b93668a22a9d644887f2cd0b5f5b969c92502
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-01-20 20:50:34 +0100

    Animated background implementation

---
+++ src/components/AnimatedBg.jsx
@@ -0,0 +1,10 @@
+import '@styles/components/AnimatedBg.css';
+export default function AnimatedBg() {
+    return (
+        <div className=" bg-bg w-full h-full absolute top-0 left-0 z-0 overflow-hidden filter blur-3xl">
+            <div className="bg-bubble"></div>
+            <div className="bg-bubble"></div>
+            <div className="bg-bubble"></div>
+        </div>
+    )
+}
