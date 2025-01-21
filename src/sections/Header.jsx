7900018ff72553583a5ba9a04cb81098a0d21123
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-01-21 13:30:10 +0100

    Add shadow to logo

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -4,8 +4,9 @@ import { FaGithub } from "react-icons/fa";
 export default function Header(){
     return (
         <header id='header' className=" z-50 absolute top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent">
-            <a href="/" className='hover:scale-110 transition-transform'>
-                <img src={Logo} alt="Utools Logo" className="w-12 h-12" />
+            <a href="/" className='hover:scale-110 transition-transform relative'>
+                <img src={Logo} alt="Utools Logo" className="w-12 h-12 z-10" />
+                <img src={Logo} alt="Utools Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
             </a>
             <nav className='flex items-center space-x-8'>
                 <a href="https://www.instagram.com/tonniggrcia_/" className='hover:scale-110 transition-transform' target='_blank' rel='noreferrer'>
