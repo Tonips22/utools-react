1124421bebb7999069d4dd1c49a81efe2c54c038
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-02-26 16:15:11 +0100

    Add filter labels on Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import AnimatedBg from '@components/AnimatedBg.jsx';
+import Label from '@components/Label.jsx';
 
 export default function Hero({ searchTerm, setSearchTerm }) {
   const handleInputChange = (event) => {
@@ -11,7 +12,9 @@ export default function Hero({ searchTerm, setSearchTerm }) {
 
       <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col items-center justify-center">
         <h1 className='hoverable text-6xl text-white font-bold cursor-default'>Utools</h1>
+
         <p className='hoverable cursor-default italic'>The best websites-tools reference in all web</p>
+
         <input
           type="text"
           value={searchTerm}
@@ -19,6 +22,21 @@ export default function Hero({ searchTerm, setSearchTerm }) {
           autoFocus
           className='hoverable searchBar text-white bg-dark py-4 px-8 rounded-full border-none outline-none w-[350px] md:w-[500px] lg:w-[600px]'
         />
+
+        <ul className="flex flex-row items-center justify-center flex-wrap gap-2 mt-4 w-[350px] md:w-[500px] lg:w-[600px]">
+          <Label hasCheckBox={true} text='Colors'/>
+          <Label hasCheckBox={true} text='Components'/>
+          <Label hasCheckBox={true} text='Icons'/>
+          <Label hasCheckBox={true} text='Typography'/>
+          <Label hasCheckBox={true} text='Design'/>
+          <Label hasCheckBox={true} text='Mockups'/>
+          <Label hasCheckBox={true} text='Deployment'/>
+          <Label hasCheckBox={true} text='Images'/>
+          <Label hasCheckBox={true} text='Videos'/>
+          <Label hasCheckBox={true} text='Hosting'/>
+          <Label hasCheckBox={true} text='API'/>
+          <Label hasCheckBox={true} text='Optimization'/>
+        </ul>
       </div>
     </section>
   );
