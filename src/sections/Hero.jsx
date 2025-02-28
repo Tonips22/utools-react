c4f88756c66107a4380f30b85b4286f1a1aae0b0
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-03-01 00:59:03 +0100

    Add search icon on searcher

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,16 +26,21 @@ export default function Hero({ searchTerm, setSearchTerm, categories, setCategor
       <AnimatedBg />
 
       <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col items-center justify-center">
-        <h1 className="hoverable text-6xl text-white font-bold cursor-default">Utools</h1>
-        <p className="hoverable cursor-default italic">The best websites-tools reference in all web</p>
+        <h1 className="text-6xl text-white font-bold cursor-default">Utools</h1>
 
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleInputChange}
-          autoFocus
-          className="hoverable searchBar text-white bg-dark py-4 px-8 rounded-full border-none outline-none w-[350px] md:w-[500px] lg:w-[600px]"
-        />
+        <p className="cursor-default italic">The best websites-tools reference in all web</p>
+
+        <label className='hoverable flex flex-row items-center gap-4 bg-dark rounded-full px-8 py-4 w-[350px] cursor-text md:w-[500px] lg:w-[600px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleInputChange}
+            autoFocus
+            className="hoverable searchBar text-white bg-transparent rounded-full border-none outline-none w-full px-1"
+          />
+        </label>
 
         <ul className="flex flex-row items-center justify-center flex-wrap gap-2 mt-4 w-[350px] md:w-[500px] lg:w-[600px]">
           {/* Por cada Label, indicamos si está checkeado 
