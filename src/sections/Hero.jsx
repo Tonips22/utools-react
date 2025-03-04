82a757d6fb29c69364c84cc9245ef66b1a7d9744
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-03-04 20:08:45 +0100

    Remove subtitle description on Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,10 +25,8 @@ export default function Hero({ searchTerm, setSearchTerm, categories, setCategor
     <section className="min-h-[75vh] flex-col items-center justify-center relative z-10" id="hero">
       <AnimatedBg />
 
-      <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col items-center justify-center">
+      <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col items-center justify-center gap-4">
         <h1 className="text-6xl text-white font-bold cursor-default">Utools</h1>
-
-        <p className="cursor-default italic">The best websites-tools reference in all web</p>
 
         <label className='hoverable flex flex-row items-center gap-4 bg-dark rounded-full px-8 py-4 w-[350px] cursor-text md:w-[500px] lg:w-[600px]'>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
@@ -42,7 +40,7 @@ export default function Hero({ searchTerm, setSearchTerm, categories, setCategor
           />
         </label>
 
-        <ul className="hoverable flex flex-row items-center justify-center flex-wrap gap-2 mt-4 w-[350px] md:w-[500px] lg:w-[600px]">
+        <ul className="hoverable flex flex-row items-center justify-center flex-wrap gap-2 w-[350px] md:w-[500px] lg:w-[600px]">
           {/* Por cada Label, indicamos si está checkeado 
               comprobando si su texto está en categories */}
           <Label
