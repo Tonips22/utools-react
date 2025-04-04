0fdcb82bba0f729d26d4d73725765e3384c2a15c
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-04-04 12:10:31 +0200

    Add auth provider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
+import { AuthProvider } from '@auth/AuthProvider.jsx';
+import '@styles/index.css'
 import '@fontsource/inder';
 import '@fontsource-variable/inter';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
