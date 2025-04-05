ad20ed60924c6df1a3416104150acaa8a88ec41e
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-04-05 16:05:53 +0200

    Add first sign in link

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
-import { FaGithub } from "react-icons/fa";
 import Coffee from '@assets/buymeacoffee.svg';
+import { AiOutlineLogin } from "react-icons/ai";
 export default function Header(){
     return (
         <header id='header' className=" z-50 absolute top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent">
@@ -11,6 +11,10 @@ export default function Header(){
                 <a href="https://buymeacoffee.com/tonigt" className='hoverable hover:scale-110 transition-transform max-w-6 relative active:scale-95' target='_blank' rel='noreferrer'>
                     <img src={Coffee} alt="BuyMeACoffee Logo" />
                     <img src={Coffee} alt="BuyMeACoffee Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
+                </a>
+                <a href="/login" className='hoverable hover:scale-110 transition-transform max-w-6 relative active:scale-95'>
+                    <AiOutlineLogin className="w-12 h-12 z-10 text-pink" />
+                    <AiOutlineLogin className="-z-10 w-12 h-12 blur-xl absolute top-0 left-0 bg-pink" />
                 </a>
             </nav>
         </header>
