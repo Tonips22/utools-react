e2d5af6aa57db145925dd037e56ee2c6fa4f0fc6
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-04-07 11:17:16 +0200

    Change Apple to Twitch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "@auth/AuthProvider.jsx";
 import AnimatedBg from "@components/AnimatedBg.jsx";
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
+import { FaTwitch } from "react-icons/fa";
 
 export default function Login() {
     const { loginWithProvider } = useAuth();
@@ -20,16 +20,16 @@ export default function Login() {
                 <h1 className="font-primary-font text-7xl">Sign In</h1>
                 <h2 className="font-bold">Choose a method to sign in.</h2>
                 <button onClick={() => loginWithProvider("google")} className="hoverable flex items-center justify-start gap-2 bg-white rounded-lg px-4 py-2 text-dark min-w-60 hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out">
-                    <FaGoogle className=" size-5 self-start" />
+                    <FaGoogle className=" size-5" />
                     <p className="self-center">Continue with Google</p>
                 </button>
                 <button onClick={() => loginWithProvider("github")} className="hoverable flex items-center justify-start gap-2 bg-white rounded-lg px-4 py-2 text-dark min-w-60 hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out">
-                    <FaGithub className=" size-5 self-start" />
+                    <FaGithub className=" size-5" />
                     <p className="self-center">Continue with GitHub</p>
                 </button>
-                <button onClick={() => loginWithProvider("apple")} className="hoverable flex items-center justify-start gap-2 bg-white rounded-lg px-4 py-2 text-dark min-w-60 hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out">
-                    <FaApple className=" size-5 self-start" />
-                    <p className="self-center">Continue with Apple</p>
+                <button onClick={() => loginWithProvider("twitch")} className="hoverable flex items-center justify-start gap-2 bg-white rounded-lg px-4 py-2 text-dark min-w-60 hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out">
+                    <FaTwitch className=" size-5" />
+                    <p className="self-center">Continue with Twitch</p>
                 </button>
             </section>
         </main>
