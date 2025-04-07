483f010804e8359d0691d5d1e28a472fb30f35ba
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-04-07 19:53:41 +0200

    Add dropdown from heroUI

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,8 +1,10 @@
 import Coffee from '@assets/buymeacoffee.svg';
 import { useAuth } from "@auth/AuthProvider.jsx";
 import { RiLoginCircleLine } from "react-icons/ri";
-import { FaUserCircle } from "react-icons/fa";
-// import { userIcon} from "@assets/user-icon.svg";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User} from "@heroui/react";
+
+
+
 export default function Header(){
     const { user, logout } = useAuth(); // Obtener el usuario autenticado desde el contexto de autenticación
     
@@ -20,37 +22,36 @@ export default function Header(){
                 </a>
                 
                 {user ?(
-                    <a href="/login" className='hoverable hover:scale-110 transition-transform max-w-6 relative active:scale-95'>
-                    {/* Contenedor cuadrado */}
-                    <div className="z-10 rounded-full overflow-hidden h-8 w-8 relative">
-                        <img
-                            src={user.user_metadata.avatar_url || "/user-icon.svg"} // Foto del avatar
-                            alt="User Avatar"
-                            className="object-cover w-full h-full" // Asegura que la imagen cubra el área y sea circular
+
+                    <Dropdown placement="bottom-end" className="hoverable z-0 rounded-xl p-2 backdrop-blur-sm bg-gray-600/20">
+                    <DropdownTrigger>
+                        <Avatar
+                            isBordered
+                            as="button"
+                            className={`hoverable relative transition-transform w-9 h-9 rounded-full hover:scale-110 active:scale-95`}
+                            src={user.user_metadata.avatar_url || "/user-icon.svg"}
                         />
-                    </div>
-                    {/* Imagen de fondo borroso */}
-                    <div className="-z-10 rounded-full overflow-hidden h-8 w-8 absolute top-0 left-0 blur-md">
-                        <img
-                            src={user.user_metadata.avatar_url || "/user-icon.svg"} // Foto del avatar
-                            alt="User Avatar"
-                            className="object-cover w-full h-full"
-                        />
-                    </div>
-                </a>
+                    </DropdownTrigger>
+                    <DropdownMenu aria-label="Profile Actions" variant="solid" className='flex flex-col gap-4 min-w-36'>
+                        <DropdownItem key="profile" className=" h-14 gap-2">
+                            <p className="text-xs text-gray-50/80">Signed in as</p>
+                            <p className="text-sm">{user.user_metadata.name || user.user_metadata.email}</p>
+                        </DropdownItem>
+                        <DropdownItem key="dashboard" className='hoverable hover:bg-white hover:text-dark transition-all duration-150 ease-out rounded-lg py-1 px-2 text-white text-sm hover:scale-105 active:scale-95' href="/dashboard">
+                        Dashboard
+                        </DropdownItem>
+                        <DropdownItem key="logout" className='hoverable hover:bg-pink hover:text-white transition-all duration-150 ease-out rounded-lg py-1 px-2 text-pink text-sm hover:scale-105 active:scale-95' onPress={logout}>
+                        Sign Out
+                        </DropdownItem>
+                    </DropdownMenu>
+                    </Dropdown>
+                    
                 ) : (
                     <a href="/login" className='hoverable flex items-center backdrop-blur-xl bg-gray-50/20  p-2 gap-2 z-10 rounded-xl hover:scale-110 transition-transform relative active:scale-95'>
                         <RiLoginCircleLine className='h-6 w-6'/>
                         <p>Sign In</p>
                     </a>
                 )}
-
-                    {/* <button
-                        onClick={logout}  // Llama a la función logout cuando se hace clic
-                        className="logout-button"
-                    >
-                        Logout
-                    </button> */}
             </nav>
         </header>
     )
