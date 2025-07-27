23b8d7f7352a3615ce85df4a2423f3e9fe372c15
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-07-27 22:02:59 +0200

    Fix dropdown heroUI styles

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,58 +1,107 @@
 import Coffee from '@assets/buymeacoffee.svg';
 import { useAuth } from "@auth/AuthProvider.jsx";
 import { RiLoginCircleLine } from "react-icons/ri";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar} from "@heroui/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@heroui/react";
 
+export default function Header({ transparent = true, absolute = true }) {
+  const { user, logout } = useAuth();
 
+  return (
+    <header
+      id="header"
+      className={`z-50 ${absolute ? "absolute" : ""} top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent ${transparent ? "" : "backdrop-blur-md"}`}
+    >
+      <a href="/" className="hoverable hover:scale-110 transition-transform relative active:scale-95">
+        <img src="/logo.webp" alt="Utools Logo" className="w-12 h-12 z-10" />
+        <img src="/logo.webp" alt="Utools Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
+      </a>
 
-export default function Header({transparent=true, absolute=true}) {
-    const { user, logout } = useAuth(); // Obtener el usuario autenticado desde el contexto de autenticación
-    
-    return (
-        <header id='header' className={` z-50 ${absolute ? "absolute" : ""} top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent ${transparent ? "" : "backdrop-blur-md"}`}>
-            <a href="/" className='hoverable hover:scale-110 transition-transform relative active:scale-95'>
-                <img src='/logo.webp' alt="Utools Logo" className="w-12 h-12 z-10" />
-                <img src='/logo.webp' alt="Utools Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
-            </a>
-            <nav className='flex items-center space-x-8'>
+      <nav className="flex items-center space-x-8">
+        <a
+          href="https://buymeacoffee.com/tonigt"
+          className="hoverable hover:scale-110 transition-transform max-w-6 relative active:scale-95"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Coffee} alt="BuyMeACoffee Logo" />
+          <img src={Coffee} alt="BuyMeACoffee Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
+        </a>
 
-                <a href="https://buymeacoffee.com/tonigt" className='hoverable hover:scale-110 transition-transform max-w-6 relative active:scale-95' target='_blank' rel='noreferrer'>
-                    <img src={Coffee} alt="BuyMeACoffee Logo" />
-                    <img src={Coffee} alt="BuyMeACoffee Logo" className=" -z-10 w-12 h-12 blur-md absolute top-0 left-0" />
-                </a>
-                
-                {user ?(
-
-                    <Dropdown placement="bottom-end" className="hoverable z-0 rounded-xl p-2 backdrop-blur-sm bg-gray-600/20">
-                    <DropdownTrigger>
-                        <Avatar
-                            isBordered
-                            as="button"
-                            className={`hoverable relative transition-transform w-9 h-9 rounded-full hover:scale-110 active:scale-95`}
-                            src={user.user_metadata.avatar_url || "/user-icon.svg"}
-                        />
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Profile Actions" variant="solid" className='flex flex-col gap-4 min-w-36'>
-                        <DropdownItem key="profile" className=" h-14 gap-2">
-                            <p className="text-xs text-gray-50/80">Signed in as</p>
-                            <p className="text-sm">{user.user_metadata.name || user.user_metadata.email}</p>
-                        </DropdownItem>
-                        <DropdownItem key="dashboard" className='hoverable hover:bg-white hover:text-dark transition-all duration-150 ease-out rounded-lg py-1 px-2 text-white text-sm hover:scale-105 active:scale-95' href="/dashboard">
-                        Dashboard
-                        </DropdownItem>
-                        <DropdownItem key="logout" className='hoverable hover:bg-pink hover:text-white transition-all duration-150 ease-out rounded-lg py-1 px-2 text-pink text-sm hover:scale-105 active:scale-95' onPress={logout}>
-                        Sign Out
-                        </DropdownItem>
-                    </DropdownMenu>
-                    </Dropdown>
+        {user ? (
+          <Dropdown
+            placement="bottom-end"
+            classNames={{
+                // wrapper del popover (el que tiene bg-content1)
+                content: "bg-transparent backdrop-blur-md rounded-xl", 
+                // puedes ocultar la flechita si quieres
+                arrow: "hidden",
+            }}
+          >
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                classNames={{
                     
-                ) : (
-                    <a href="/login" className='hoverable flex items-center backdrop-blur-xl bg-gray-50/20  p-2 gap-2 z-10 rounded-xl hover:scale-110 transition-transform relative active:scale-95'>
-                        <RiLoginCircleLine className='h-6 w-6'/>
-                        <p>Sign In</p>
-                    </a>
-                )}
-            </nav>
-        </header>
-    )
+                  base: "hoverable relative transition-transform w-9 h-9 rounded-full hover:scale-110 active:scale-95 cursor-pointer",
+                  image: "rounded-full object-cover w-full h-full",
+
+                }}
+                src={user.user_metadata.avatar_url || "/user-icon.svg"}
+              />
+            </DropdownTrigger>
+
+            <DropdownMenu
+              aria-label="Profile Actions"
+              variant="light"
+              classNames={{
+                base: " rounded-xl bg-dark backdrop-blur-sm p-2", // contenedor principal
+                list: "flex flex-col gap-2",                             // lista UL interna
+              }}
+              itemClasses={{
+                base: "rounded-lg py-1 px-2 text-sm text-white hover:scale-105 active:scale-95 transition-all duration-200 ease-in-out",
+              }}
+            >
+              <DropdownItem key="profile" textValue="Profile info" isReadOnly classNames={{ base: "h-14 gap-2 bg-transparent hover:scale-100 cursor-default" }}>
+                <p className="text-xs text-white/80">Signed in as</p>
+                <p className="text-sm text-white">{user.user_metadata.name || user.user_metadata.email}</p>
+              </DropdownItem>
+
+              <DropdownItem
+              key="dashboard"
+              textValue="Dashboard"
+              href="/dashboard"
+              classNames={{
+                base: "group bg-transparent hover:bg-white",
+                title: "text-sm text-white group-hover:text-dark",
+              }}
+            >
+              Dashboard
+            </DropdownItem>
+
+              <DropdownItem
+                key="logout"
+                textValue="Sign out"
+                classNames={{
+                  base: "group bg-transparent hover:bg-pink",
+                  title: "text-sm text-pink group-hover:text-dark",
+                }}
+                onPress={logout}
+              >
+                Sign Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        ) : (
+          <a
+            href="/login"
+            className="hoverable flex items-center backdrop-blur-xl bg-gray-50/20  p-2 gap-2 z-10 rounded-xl hover:scale-110 transition-transform relative active:scale-95"
+          >
+            <RiLoginCircleLine className="h-6 w-6" />
+            <p>Sign In</p>
+          </a>
+        )}
+      </nav>
+    </header>
+  );
 }
