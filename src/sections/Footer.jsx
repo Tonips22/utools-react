2d78e40488cb02e9c20157c829fc93ab8cd2f0bf
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-07-31 12:50:27 +0200

    New footer size

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,10 +5,13 @@ import { SiBuymeacoffee } from "react-icons/si";
 export default function Footer() {
     const year = new Date().getFullYear();
     return (
-        <footer id='footer' className='relative z-50 w-full flex justify-center items-center py-4 px-4 bg-transparent min-h-[25vh]'>
-            <AnimatedBg />
+        <footer id='footer' className='relative z-50 w-full flex justify-center items-center py-4 px-4 bg-transparent min-h-[10vh]'>
+            <AnimatedBg isFooter={true} />
             <div className='absolute top-0 left-0 h-full flex flex-row items-end justify-between px-8 py-4 w-full'>
-                <h2 className=' text-lg font-primary'>&copy;Utools {year}</h2>
+                <h2 className=' text-sm font-secondary'>
+                    &copy;Utools
+                    <span className='text-xs text-transparent bg-clip-text bg-gradient-to-r from-light-blue via-purple to-pink'> {year}</span>
+                    </h2>
 
                 <div className='flex flex-row text-sm items-center justify-center gap-4 *:transition-all *:duration-200 *:ease'>
                     <a href='/privacy' target='_blank' rel='noreferrer' className='hoverable hover:scale-110 active:scale-95'>
