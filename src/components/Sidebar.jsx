4523a685910c89f12fc182796fde4dd8438ae431
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2026-01-24 13:50:00 +0100

    refactor: replace search icon with RiSearchLine and clean up button styles in Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { RxDashboard } from "react-icons/rx";
 import { FaHome } from "react-icons/fa";
 import { useAuth } from "@auth/AuthProvider.jsx";
-import { CiSearch } from "react-icons/ci";
+import { RiSearchLine} from 'react-icons/ri';
 import Button from "@components/Button.jsx";
 
 export default function Sidebar() {
@@ -16,23 +16,15 @@ export default function Sidebar() {
         <nav className="flex flex-col items-center gap-4">
             <Button
                 onClick={() => navigate("/")}
-                className={`rounded-full p-2 w-14 h-14 ${
-                    location.pathname === "/"
-                        ? "bg-white text-dark"
-                        : "text-white"
-                }`}
+                className={`rounded-full p-2 w-14 h-14`}
             >
                 <FaHome className="text-lg" />
             </Button>
             <Button
                 onClick={() => navigate("/search")}
-                className={`rounded-full p-2 w-14 h-14 ${
-                    location.pathname === "/search"
-                        ? "bg-white text-dark"
-                        : "text-white"
-                }`}
+                className={`rounded-full p-2 w-14 h-14`}
             >
-                <CiSearch className="text-lg font-bold" />
+                <RiSearchLine className="text-lg font-bold" />
             </Button>
             <Button
                 onClick={() => navigate("/dashboard/my-posts")}
@@ -50,7 +42,7 @@ export default function Sidebar() {
             {user && (
             <NavLink
                 to="/dashboard/profile"
-                className={({ isActive }) =>`hoverable relative block transition-transform rounded-full hover:scale-110 active:scale-95 ${isActive ? "ring-2 ring-offset-2 ring-white/50 ring-offset-bg" : ""
+                className={({ isActive }) =>`hoverable relative block transition-transform rounded-full ${isActive ? "ring-2 ring-offset-2 ring-white/50 ring-offset-bg" : "hover:scale-110 active:scale-95"
                 }`
                 }
             >
